0b7d39c532e1d19dc2143ff8ec675e6df97db5d6
Natasha Gonzalez <82616634+natgonzalez2329@users.noreply.github.com>
2021-07-12 23:12:40 -0300

    forms paleta de colores, select, adaptaciones responsive

--- src/main.js
+++ src/main.js
@@ -4,15 +4,14 @@ import { myFunction } from './lib/index.js';
 
 myFunction();
 
-
-const sign_in_btn = document.querySelector('#sign-in-btn');
-const sign_up_btn = document.querySelector('#sign-up-btn');
+const signInBtn = document.querySelector('#sign-in-btn');
+const signUpBtn = document.querySelector('#sign-up-btn');
 const container = document.querySelector('.container');
 
-sign_up_btn.addEventListener('click', () => {
+signUpBtn.addEventListener('click', () => {
   container.classList.add('sign-up-mode');
 });
 
-sign_in_btn.addEventListener('click', () => {
+signInBtn.addEventListener('click', () => {
   container.classList.remove('sign-up-mode');
 });
