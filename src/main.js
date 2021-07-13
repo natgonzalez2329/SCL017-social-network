d9c37ab3b76fb7e21aef3055cd44a3924b1ff33a
Natasha Gonzalez <82616634+natgonzalez2329@users.noreply.github.com>
2021-07-13 11:28:59 -0300

    readme prototipo baja y alta fidelidad hu1

--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
 // Este es el punto de entrada de tu aplicacion
-import { router } from './router/routers.js';
 import { myFunction } from './lib/index.js';
 
 myFunction();
 
-window.addEventListener('hashchange', () => {
-  console.log(window.location.hash);
-});
-
-const router = (route) => {
-  console.log(hj);
-};
 const signInBtn = document.querySelector('#sign-in-btn');
 const signUpBtn = document.querySelector('#sign-up-btn');
 const container = document.querySelector('.container');
@@ -22,6 +14,3 @@ signUpBtn.addEventListener('click', () => {
 signInBtn.addEventListener('click', () => {
   container.classList.remove('sign-up-mode');
 });
-
-
-
