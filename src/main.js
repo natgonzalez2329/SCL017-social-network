26567816482847c1c62af047fa7e7da209f2075c
Natasha Gonzalez <82616634+natgonzalez2329@users.noreply.github.com>
2021-07-12 23:08:14 -0300

    corrección js

--- src/main.js
+++ src/main.js
@@ -5,14 +5,14 @@ import { myFunction } from './lib/index.js';
 myFunction();
 
 
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
+const sign_in_btn = document.querySelector('#sign-in-btn');
+const sign_up_btn = document.querySelector('#sign-up-btn');
+const container = document.querySelector('.container');
 
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
+sign_up_btn.addEventListener('click', () => {
+  container.classList.add('sign-up-mode');
 });
 
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
+sign_in_btn.addEventListener('click', () => {
+  container.classList.remove('sign-up-mode');
 });
