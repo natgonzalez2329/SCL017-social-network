df7a3b3c70736930cbe4f97194c4e0c9e0cf8307
Natasha Gonzalez <82616634+natgonzalez2329@users.noreply.github.com>
2021-07-13 09:17:59 -0300

    test

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 // Este es el punto de entrada de tu aplicacion
-
 import { myFunction } from './lib/index.js';
 
 myFunction();
 
+window.addEventListener('hashchange', () => {
+  console.log(window.location.hash);
+});
+
+const router = (route) => {
+  console.log(hj);
+};
 const signInBtn = document.querySelector('#sign-in-btn');
 const signUpBtn = document.querySelector('#sign-up-btn');
 const container = document.querySelector('.container');
@@ -15,3 +21,5 @@ signUpBtn.addEventListener('click', () => {
 signInBtn.addEventListener('click', () => {
   container.classList.remove('sign-up-mode');
 });
+
+
