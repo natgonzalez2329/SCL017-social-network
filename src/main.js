ec99dc704882d408850e0c3af5e8e37bba6da884
Natasha Gonzalez <82616634+natgonzalez2329@users.noreply.github.com>
2021-07-16 15:22:21 -0300

    folder style

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // Este es el punto de entrada de tu aplicacion
 import { myFunction } from './lib/index.js';
 import { init } from './lib/init.js';
+
 myFunction();
 window.addEventListener('load', init);
