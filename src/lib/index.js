775e80c68d59d439b149569b9cf658a0a7a3b968
ViviMoya <v.moyacapitan@gmail.com>
2021-07-16 22:14:55 -0400

    se agrega link de terminos y condiciones y acceso con facebook parte 1

--- src/lib/index.js
+++ src/lib/index.js
@@ -35,11 +35,46 @@ const googleLogin = () => {
 
 }
 
-
-
 const googleLoginBtn = document.getElementById("google_login");
  googleLoginBtn.addEventListener('click', ()=>{
           googleLogin();
  });
 
-export { myFunction, googleLogin};
+
+ const FacebookLogin = () => {
+  const provider = new firebase.auth.FacebookAuthProvider();
+ firebase
+ .auth()
+ .signInWithPopup(provider)
+ .then((result) => {
+   /** @type {firebase.auth.OAuthCredential} */
+   var credential = result.credential;
+
+   // The signed-in user info.
+   var user = result.user;
+
+   // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+   var accessToken = credential.accessToken;
+
+   // ...
+ })
+ .catch((error) => {
+   // Handle Errors here.
+   var errorCode = error.code;
+   var errorMessage = error.message;
+   // The email of the user's account used.
+   var email = error.email;
+   // The firebase.auth.AuthCredential type that was used.
+   var credential = error.credential;
+
+   // ...
+ });
+}
+
+const facebookLoginBtn = document.getElementById("facebook_login");
+facebookLoginBtn.addEventListener('click', ()=>{
+  FacebookLogin();
+ });
+
+
+export { myFunction, googleLogin, FacebookLogin};
