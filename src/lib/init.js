7b3a713a6608bdc204256873b2751c21e73cdfda
Hiliana Angulo <ahiliana@gmail.com>
2021-07-27 17:28:54 -0400

    modificaciones en init

--- src/lib/init.js
+++ src/lib/init.js
@@ -1,10 +1,8 @@
 import { router } from './routers.js';
-//import { viewHeaderMenu } from '../views/headerMenu.js';
 import { viewForms } from '../views/viewForms.js';
 
 export const init = () => {
-  
-  //document.getElementById('root').innerHTML = '';
+  document.getElementById('root').innerHTML = '';
   document.getElementById('root').appendChild(viewForms());
   window.location.hash = '/';
   window.addEventListener('hashchange', () => {
