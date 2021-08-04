3561efa52fcbf27925940a8be7c3735d32791272
Hiliana Angulo <ahiliana@gmail.com>
2021-08-04 09:04:07 -0400

    Se aplica CSS en barra de menu

--- src/lib/init.js
+++ src/lib/init.js
@@ -7,10 +7,10 @@ import { viewPost } from '../views/viewPost.js';
 const containerViews = document.querySelector('#root');
 
 export const init = () => {
-  document.getElementById('root').innerHTML = '';
-  document.getElementById('root').appendChild(viewForms());
+  containerViews.innerHTML = '';
+  containerViews.appendChild(viewForms());
   window.addEventListener('hashchange', () => {
-    document.getElementById('root').innerHTML = '';
+    containerViews.innerHTML = '';
     router(window.location.hash);
     
     
@@ -22,19 +22,19 @@ const currentPath = window.location.hash;
 switch (currentPath) {
   case '#feed':
     // document.getElementById('root').appendChild(standardTemplate());
-    document.getElementById('root').appendChild(viewFeed()); // ruta muro posts
+    containerViews.appendChild(viewFeed()); // ruta muro posts
     
     break;
     case '#profile':
-      document.getElementById('root').innerHTML = '';
-      document.getElementById('root').appendChild(viewProfile()); // ruta perfil
+      containerViews.innerHTML = '';
+      containerViews.appendChild(viewProfile()); // ruta perfil
       break;
       case '#post':
-        document.getElementById('root').innerHTML = '';
-        document.getElementById('root').appendChild(viewPost()); // ruta post
+        containerViews.innerHTML = '';
+        containerViews.appendChild(viewPost()); // ruta post
         break;
         default:
-          document.getElementById('root').innerHTML = 'Error 404';
+          containerViews.innerHTML = 'Error 404';
           break;
         }
 
